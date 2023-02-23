417cbd07101c2785c092c13e7e0b79b6abe4edf2
mike.dev <menglu95120@gmail.com>
2023-02-24 00:13:33 +0800

    WIP article page

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, FC } from 'react';
 import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 import { TNewsData } from '../common';
 import { Loading } from '../components';
+import parser from 'html-react-parser';
 
 const Article: FC = () => {
   const location = useLocation();
@@ -15,17 +17,48 @@ const Article: FC = () => {
     <>
       {data === undefined ?
         <Loading /> :
-        <div>
-          <div>{data.webPublicationDate}</div>
-          <div>{data.webTitle}</div>
-          <div>{data.headline}</div>
-          <div>{data.body}</div>
-          <div>{data.thumbnail}</div>
-        </div>
+        <Container>
+          <Content>
+            <DateTime>{data.webPublicationDate}</DateTime>
+            <Title>{data.webTitle}</Title>
+            <Headline>{data.headline}</Headline>
+            <Detail>{parser(data.body)}</Detail>
+          </Content>
+          {data.thumbnail && <Media src={data.thumbnail} />}
+        </Container>
       }
     </>
   )
 }
+
+const DateTime = styled.div``;
+
+const Title = styled.div``;
+
+const Headline = styled.div``;
+
+const Detail = styled.div`
+
+`;
+
+const Media = styled.div<{ src: string }>`
+  background-image: url(${props => props.src});
+  width: 445px;
+  height: 267px;
+  background-size: 100% 100%;
+`;
+
+const Content = styled.div`
+  width: 635px;
+  margin: 30px;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
 
 export { Article };
 export default Article;
