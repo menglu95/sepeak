9cdb217c2ff3ac8310e38d631ecc52faf78b15f6
mike.dev <menglu95120@gmail.com>
2023-02-28 22:52:48 +0800

    correct key

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
 
 export const API_ENDPOINT = 'https://content.guardianapis.com/search?show-fields=headline%2Cthumbnail%2Cbody%2CbodyText&show-elements=all';
 export const API_KEY = '347d2d3b-47eb-46cc-a762-103c626745db';
-// export const API_KEY = '09b3f67b-977f-49ab-bcb8-d94469d2825e';
